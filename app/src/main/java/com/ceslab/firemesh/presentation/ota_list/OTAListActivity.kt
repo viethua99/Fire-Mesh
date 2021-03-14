f4e0147c03bb811965301c62cc22c8bf6c3fcb14
Viet Hua <huaviet999@gmail.com>
2021-03-14 11:33:10 +0700

    ota list UI fix

--- app/src/main/java/com/ceslab/firemesh/presentation/ota_list/OTAListActivity.kt
+++ app/src/main/java/com/ceslab/firemesh/presentation/ota_list/OTAListActivity.kt
@@ -26,6 +26,8 @@ import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.fragment_ota_list.*
 import kotlinx.android.synthetic.main.fragment_ota_list.btn_scanning
 import kotlinx.android.synthetic.main.fragment_ota_list.looking_for_devices_background
+import kotlinx.android.synthetic.main.fragment_ota_list.tv_scanning_message
+import kotlinx.android.synthetic.main.fragment_provision_list.*
 import timber.log.Timber
 
 class OTAListActivity : BaseActivity(), Discovery.BluetoothDiscoveryHost,
@@ -219,9 +221,11 @@ class OTAListActivity : BaseActivity(), Discovery.BluetoothDiscoveryHost,
             discovery.stopDiscovery(false)
             scanning = false
             btn_scanning.text =getString(R.string.fragment_provision_list_start_scanning)
+            tv_scanning_message.text = getString(R.string.fragment_ota_list_press_start_message)
             btn_scanning.setBackgroundColor(Color.parseColor("#0288D1"))
         } else {
             btn_scanning.text = getString(R.string.fragment_provision_list_stop_scanning)
+            tv_scanning_message.text = getString(R.string.fragment_ota_list_looking_for_nearby_devices)
             btn_scanning.setBackgroundColor(Color.parseColor("#F44336"))
             startScanning()
         }
